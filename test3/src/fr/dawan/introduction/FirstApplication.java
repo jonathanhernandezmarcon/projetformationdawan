a9169d532b9888f64eee88935154e17733daed74
hernandezjonathanmarcon <hernandezjo@hotmail.fr>
2022-06-23 10:34:49 +0200

    Add files via upload

---
+++ test3/src/fr/dawan/introduction/FirstApplication.java
@@ -0,0 +1,5 @@
+package fr.dawan.introduction;
+
+public class FirstApplication {
+
+}
---
+++ test3/src/fr/dawan/tableau/tableau.java
@@ -0,0 +1,17 @@
+package fr.dawan.tableau;
+
+public class tableau {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double [] = {};
+		
+		double [] test= input.double();
+		
+		for (int i=0; i<test.length; i++) {
+			
+		}
+
+	}
+
+}
